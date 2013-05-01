40a19e0d2885721671f2000326580b8913f4dcd1
Jaco de Groot <jaco.de.groot@ibissource.org>
2013-05-01 13:52:15 +0200

    Initial version

---
+++ src/main/java/com/ibm/mq/jms/JMSC.java
@@ -0,0 +1,6 @@
+package com.ibm.mq.jms;
+
+public class JMSC {
+	public static final int MQJMS_CLIENT_NONJMS_MQ = 0;
+	public static final int MQJMS_CLIENT_JMS_COMPLIANT = 1;
+}
---
+++ src/main/java/com/ibm/mq/jms/MQQueue.java
@@ -0,0 +1,6 @@
+package com.ibm.mq.jms;
+
+public class MQQueue {
+	public void setTargetClient(int i) {
+	}
+}
